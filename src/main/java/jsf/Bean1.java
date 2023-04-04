6022b3772c2c8be409b0ecdffd3b7674e3de05cc
Riantsoa Ramanampamonjy <ramanampamonjysandratra@gmail.com>
2023-04-04 22:59:10 +0300

    exo3 et 4

--- src/main/java/jsf/Bean1.java
+++ src/main/java/jsf/Bean1.java
@@ -4,21 +4,26 @@
  */
 package jsf;
 
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Named;
 import java.util.ArrayList;
 import java.util.List;
-import jakarta.inject.Named;
-import jakarta.enterprise.context.RequestScoped;
 
-@Named(value = "bean")
+
+@Named(value = "bean1")
 @RequestScoped
-public class Bean {
+public class Bean1 {
 
-    private int nombre;
+   private int nombre;
 
     public int getNombre() {
         return nombre;
     }
 
+    public String afficher() {
+        return "affichage_4?nb="+ nombre + "&amp;faces-redirect=true";
+    }
+    
     public void setNombre(int nombre) {
         this.nombre = nombre;
     }
@@ -31,8 +36,5 @@ public class Bean {
         }
         return l;
     }
-
-    public String afficher() {
-        return "affichage_3?nb=" + nombre + "&amp;faces-redirect=true";
-    }
+    
 }
